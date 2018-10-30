44c5efaa75977623ec531be0f928681ebc8118c0
Jinyan <jinyan.shen21@gmail.com>
2018-10-30 18:45:16 -0400

    feat(api routes): Delete an order route
    Delete got order by userId route
    
    Closes #17

--- server/api/orders.js
+++ server/api/orders.js
@@ -12,20 +12,6 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-//Get order By userid
-router.get('/:userId', async (req, res, next) => {
-  try {
-    const singleOrder = await Order.findAll({
-      where: {
-        userId: +req.params.userId
-      }
-    })
-    res.status(200).json(singleOrder)
-  } catch (err) {
-    next(err)
-  }
-})
-
 //Post a order
 router.post('/', async (req, res, next) => {
   try {
@@ -37,6 +23,24 @@ router.post('/', async (req, res, next) => {
       userId: req.body.userId
     })
     res.status(201).json(newOrder)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:orderId', async (req, res, next) => {
+  try {
+    const singleOrder = await Order.findById(+req.params.orderId)
+    if (!singleOrder) {
+      res.sendStatus(404)
+    } else {
+      const updatedOrder = await singleOrder.update({
+        where: {
+          status: req.body.staus
+        }
+      })
+      res.status(201).json(updatedOrder)
+    }
   } catch (err) {
     next(err)
   }
