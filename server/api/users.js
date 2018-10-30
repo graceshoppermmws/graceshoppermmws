a33076a59984ca0c45f6ae9f27b1418945492ee7
Hystee <hysteebyhystee@gmail.com>
2018-10-30 19:20:32 -0400

    feat(routes): Add Eager Loading to Routes
    Add eager loading to user route including orders and clean up order routes
    
    Closes #35

--- server/api/users.js
+++ server/api/users.js
@@ -1,6 +1,23 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const {User, Order} = require('../db/models')
 module.exports = router
+
+// get one user and eager load orders
+
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const particularUser = await User.findById(+req.params.userId, {
+      include: [{model: Order}]
+    })
+    if (!particularUser) {
+      res.sendStatus(404)
+    } else {
+      res.status(200).json(particularUser)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
 
 router.get('/', async (req, res, next) => {
   try {
