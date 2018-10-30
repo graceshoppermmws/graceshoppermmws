ba9b2910829ed2d15e9b193e57c0fb4fc65d582a
Jinyan <jinyan.shen21@gmail.com>
2018-10-30 17:09:35 -0400

    feat (routes order
    order.get all/one, order.post
    
    in process #17

---
+++ server/api/orders.js
@@ -0,0 +1,41 @@
+const router = require('express').Router()
+const {User, Order} = require('../db/models')
+module.exports = router
+
+//Get all orders
+router.get('/', async (req, res, next) => {
+  try {
+    const allOrders = await Order.findAll()
+    res.status(200).json(allOrders)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//Get order By userid
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const singleOrder = await Order.findAll({
+      where: {
+        userId: +req.params.userId
+      }
+    })
+    res.status(200).json(singleOrder)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//Post a order
+router.post('/', async (req, res, next) => {
+  try {
+    const newOrder = await Order.create({
+      status: req.body.status,
+      historicPrice: req.body.historicPrice,
+      quantity: req.body.quantity,
+      candidateId: req.body.candidateId
+    })
+  } catch (err) {
+    next(err)
+  }
+})
