97a4d640920e9683861f2ed4b6b2f1a40fa964b8
Jinyan <jinyan.shen21@gmail.com>
2018-10-31 13:04:16 -0400

    feat(react component): react component allRaces
    Finished react component allRaces
    
    Closes #22

--- client/routes.js
+++ client/routes.js
@@ -30,7 +30,7 @@ class Routes extends Component {
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
-        <Route component={Login} />
+        <Route component={AllRaces} />
       </Switch>
     )
   }
