a1d80701d7906813db382e7d1140a05296ad70e1
kaitlinmaier <kaitlin.e.maier@gmail.com>
2018-10-31 12:30:14 -0400

    feat(store): Create candidate and race reducers
    Progess on #19

---
+++ client/store/candidates.js
@@ -0,0 +1,83 @@
+import axios from 'axios'
+
+/**
+ * ACTION TYPES
+ */
+const ADD_CANDIDATE = 'ADD_CANDIDATE'
+const EDIT_CANDIDATE = 'EDIT_CANDIDATE'
+
+/**
+ * INITIAL STATE
+ */
+const defaultCandidatesState = []
+
+/**
+ * ACTION CREATORS
+ */
+export const createCandidate = candidate => ({
+  type: ADD_CANDIDATE,
+  candidate
+})
+
+export const editCandidate = (candidateId, candidate) => ({
+  type: EDIT_CANDIDATE,
+  candidateId,
+  candidate
+})
+
+/**
+ * THUNK CREATORS
+ */
+export const postCandidate = (raceId, candidate) => {
+  return async dispatch => {
+    try {
+      const response = await axios.post(
+        `api/races/${raceId}/candidates`,
+        candidate
+      )
+      const newCandidate = response.data
+      const action = createCandidate(newCandidate)
+      dispatch(action)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
+
+export const updateCandidate = (raceId, candidateId, candidate) => {
+  return async dispatch => {
+    try {
+      const response = await axios.put(
+        `api/races/${raceId}/candidates/${candidateId}`,
+        candidate
+      )
+      const updatedCandidate = response.data
+      const action = editCandidate(candidateId, updatedCandidate)
+      dispatch(action)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = defaultCandidatesState, action) {
+  switch (action.type) {
+    case ADD_CANDIDATE: {
+      return [...state, action.candidate]
+    }
+    case EDIT_CANDIDATE: {
+      const editedCandidates = state.candidates.map(candidate => {
+        if (candidate.id === action.candidateId) {
+          return action.candidate
+        }
+        return candidate
+      })
+      return [...state, editedCandidates]
+    }
+    default:
+      return state
+  }
+}
---
+++ client/store/races.js
@@ -0,0 +1,126 @@
+import axios from 'axios'
+
+/**
+ * ACTION TYPES
+ */
+const GET_RACES = 'GET_RACES'
+const SELECT_RACE = 'SELECT_RACE'
+const ADD_RACE = 'ADD_RACE'
+const EDIT_RACE = 'EDIT_RACE'
+
+/**
+ * INITIAL STATE
+ */
+const defaultRaceState = {
+  allRaces: [],
+  selectedRace: {}
+}
+
+/**
+ * ACTION CREATORS
+ */
+export const getRaces = races => ({
+  type: GET_RACES,
+  races
+})
+
+export const selectRace = race => ({
+  type: SELECT_RACE,
+  race
+})
+
+export const createRace = race => ({
+  type: ADD_RACE,
+  race
+})
+
+export const editRace = (raceId, race) => ({
+  type: EDIT_RACE,
+  raceId,
+  race
+})
+
+/**
+ * THUNK CREATORS
+ */
+
+export const fetchRaces = () => {
+  return async dispatch => {
+    try {
+      const response = await axios.get('/api/races')
+      const races = response.data
+      const action = getRaces(races)
+      dispatch(action)
+    } catch (err) {
+      // to add toastr
+      console.error(err)
+    }
+  }
+}
+
+export const fetchSelectedRace = raceId => {
+  return async dispatch => {
+    try {
+      const response = await axios.get(`/api/races/${raceId}`)
+      const race = response.data
+      const action = selectRace(race)
+      dispatch(action)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
+
+export const postRace = race => {
+  return async dispatch => {
+    try {
+      const response = await axios.post('api/races', race)
+      const newRace = response.data
+      const action = createRace(newRace)
+      dispatch(action)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
+
+export const updateRace = (raceId, race) => {
+  return async dispatch => {
+    try {
+      const response = await axios.put(`api/races/${raceId}`, race)
+      const updatedRace = response.data
+      const action = editRace(updatedRace)
+      dispatch(action)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = defaultRaceState, action) {
+  switch (action.type) {
+    case GET_RACES: {
+      return {...state, allRaces: action.races}
+    }
+    case SELECT_RACE: {
+      return {...state, selectedRace: action.race}
+    }
+    case ADD_RACE: {
+      return {...state, allRaces: [...state.allRaces, action.race]}
+    }
+    case EDIT_RACE: {
+      const editedRaces = state.allRaces.map(race => {
+        if (race.id === action.raceId) {
+          return action.race
+        }
+        return race
+      })
+      return {...state, allRaces: editedRaces}
+    }
+    default:
+      return state
+  }
+}
