0aee470efc8103fa87e51916f92b8eaa3757b27f
Olivia Ehrenreich Marks <oehrenmarks@gmail.com>
2018-10-31 19:52:15 -0400

    feat(newModel): added product model and changed associations (#59)
    * feat(newModel): added product model and changed associatons
    
    closes #53
    
    * feat(newProperty): Add goveLevel prop to product model
    
    closes #53

---
+++ server/db/models/product.js
@@ -0,0 +1,39 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Poduct = db.define('candidate', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  bio: {
+    type: Sequelize.TEXT
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue:
+      'http://theotherway.org/wp-content/uploads/2014/08/Coming-soon.jpg'
+  },
+  districtName: {
+    type: Sequelize.STRING
+  },
+  position: {
+    type: Sequelize.STRING
+  },
+  inventory: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.DECIMAL
+  },
+  govLevel: {
+    type: Sequelize.ENUM('Municipal', 'State', 'Federal'),
+    allowNull: false
+  }
+})
+
+module.exports = Poduct
