5f32d1d0df7aa3e372b8c27fe0f0bce40ee5e0f8
kaitlinmaier <kaitlin.e.maier@gmail.com>
2018-11-01 12:32:36 -0400

    feat(component): Update Edit and Add Admin vs. Shopper View
    Add Admin vs. Shopper filter from store to react component
    
    Progress on #77

--- client/components/AddProduct.js
+++ client/components/AddProduct.js
@@ -1,20 +1,23 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {postProduct} from '../store/products'
+import ProductForm from './ProductForm'
+
+const defaultState = {
+  name: '',
+  bio: '',
+  imageUrl: '',
+  districtName: '',
+  position: '',
+  inventory: '',
+  price: '',
+  govLevel: ''
+}
 
 class AddProduct extends Component {
   constructor() {
     super()
-    this.state = {
-      name: '',
-      bio: '',
-      imageUrl: '',
-      districtName: '',
-      postition: '',
-      inventory: 0,
-      price: 0,
-      govLevel: ''
-    }
+    this.state = defaultState
     this.changeHandler = this.changeHandler.bind(this)
     this.submitHandler = this.submitHandler.bind(this)
   }
@@ -28,60 +31,16 @@ class AddProduct extends Component {
   async submitHandler(event) {
     event.preventDefault()
     const newProduct = await this.props.postProduct(this.state)
-    console.log(newProduct)
+    this.setState(defaultState)
   }
 
   render() {
     return (
-      <form onSubmit={this.submitHandler} name={name}>
-        <div>
-          <label htmlFor="name">
-            <small>Name</small>
-            <input
-              name="name"
-              type="text"
-              value={this.state.name}
-              onChange={this.changeHandler}
-            />
-          </label>
-          <label htmlFor="bio">
-            <small>Bio</small>
-            <input
-              name="bio"
-              type="text"
-              value={this.state.bio}
-              onChange={this.changeHandler}
-            />
-          </label>
-          <label htmlFor="district">
-            <small>District</small>
-            <input
-              name="district"
-              type="text"
-              value={this.state.district}
-              onChange={this.changeHandler}
-            />
-          </label>
-          <label htmlFor="inventory">
-            <small>Inventory</small>
-            <input
-              name="inventory"
-              type="text"
-              value={this.state.inventory}
-              onChange={this.changeHandler}
-            />
-          </label>
-          <label htmlFor="govLevel">
-            <small>Level</small>
-            <select name="govLevel" onChange={this.changeHandler}>
-              <option value="Municipal">Municipal</option>
-              <option value="State">State</option>
-              <option value="Federal">Federal</option>
-            </select>
-          </label>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      <ProductForm
+        product={this.state}
+        changeHandler={this.changeHandler}
+        submitHandler={this.submitHandler}
+      />
     )
   }
 }
---
+++ client/components/ProductForm.js
@@ -0,0 +1,86 @@
+import React from 'react'
+
+const ProductForm = props => {
+  return (
+    <form onSubmit={props.submitHandler} name={name}>
+      <div>
+        <label htmlFor="name">
+          <small>Name</small>
+          <input
+            name="name"
+            type="text"
+            value={props.product.name}
+            onChange={props.changeHandler}
+          />
+        </label>
+        <label htmlFor="imageUrl">
+          <small>Image</small>
+          <input
+            name="imageUrl"
+            type="text"
+            value={props.product.imageUrl}
+            onChange={props.changeHandler}
+          />
+        </label>
+        <label htmlFor="bio">
+          <small>Bio</small>
+          <input
+            name="bio"
+            type="text"
+            value={props.product.bio}
+            onChange={props.changeHandler}
+          />
+        </label>
+        <label htmlFor="district">
+          <small>District</small>
+          <select name="districtName" onChange={props.changeHandler}>
+            <option value="US House of Representatives District 14">
+              US House of Representatives District 14
+            </option>
+            <option value="State Senate District 17">
+              State Senate District 17
+            </option>
+          </select>
+        </label>
+        <label htmlFor="position">
+          <small>Position</small>
+          <input
+            name="position"
+            type="text"
+            value={props.product.position}
+            onChange={props.changeHandler}
+          />
+        </label>
+        <label htmlFor="inventory">
+          <small>Inventory</small>
+          <input
+            name="inventory"
+            type="text"
+            value={props.product.inventory}
+            onChange={props.changeHandler}
+          />
+        </label>
+        <label htmlFor="price">
+          <small>Price</small>
+          <input
+            name="price"
+            type="text"
+            value={props.product.price}
+            onChange={props.changeHandler}
+          />
+        </label>
+        <label htmlFor="govLevel">
+          <small>Level</small>
+          <select name="govLevel" onChange={props.changeHandler}>
+            <option value="Municipal">Municipal</option>
+            <option value="State">State</option>
+            <option value="Federal">Federal</option>
+          </select>
+        </label>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+export default ProductForm
