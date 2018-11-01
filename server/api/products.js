4728395cfcd99f59a7ed1a1b317d05dcb703c8f2
hystee <29220493+hystee@users.noreply.github.com>
2018-10-31 20:33:36 -0400

    Rewrite routes #54 (#61)
    * feat(routes): pause for merge
    
    * fix(routes): Rewrite routes to use new models
    
    Create Product routes and clean up order routes
    
    Closes #54
    
    * fix(routes): Rewrite routes Add index of api
    
    Added /products and /orders back to index file in API
    
    #54
    
    * fix(routes): fix cart status
    
    fix status to Cart in order routes
    
    #54

---
+++ server/api/products.js
@@ -0,0 +1,92 @@
+const router = require('express').Router()
+const {Product} = require('../db/models')
+module.exports = router
+
+// api/products
+
+// GET all Products
+router.get('/', async (req, res, next) => {
+  try {
+    const allProducts = await Product.findAll()
+    res.status(200).json(allProducts)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// GET by ProductId
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const oneProduct = await Product.findById(+req.params.productId)
+    if (!oneProduct) {
+      res.sendStatus(404)
+    } else {
+      res.status(200).json(oneProduct)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+// POST a Product
+router.post('/', async (req, res, next) => {
+  try {
+    const {
+      name,
+      bio,
+      imageUrl,
+      districtName,
+      position,
+      inventory,
+      price,
+      govLevel
+    } = req.body
+    const newProduct = await Product.create({
+      name,
+      bio,
+      imageUrl,
+      districtName,
+      position,
+      inventory,
+      price,
+      govLevel
+    })
+    res.status(201).json(newProduct)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// PUT by ProductId
+router.put('/:ProductId', async (req, res, next) => {
+  try {
+    const oldProduct = await Product.findById(+req.params.ProductId)
+    if (!oldProduct) {
+      res.sendStatus(404)
+    } else {
+      const {
+        name,
+        bio,
+        imageUrl,
+        districtName,
+        position,
+        inventory,
+        price,
+        govLevel
+      } = req.body
+      const updatedProduct = oldProduct.update({
+        name,
+        bio,
+        imageUrl,
+        districtName,
+        position,
+        inventory,
+        price,
+        govLevel
+      })
+      res.status(201).json(updatedProduct)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
