4cd17f18ad677b562f2c7aecb1836a19f0f5bb03
Hystee <hysteebyhystee@gmail.com>
2018-10-31 20:30:59 -0400

    fix(routes): Rewrite routes Add index of api
    Added /products and /orders back to index file in API
    
    #54

--- server/api/index.js
+++ server/api/index.js
@@ -2,9 +2,10 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
-// router.use('/orders', require('./orders'))
+router.use('/orders', require('./orders'))
 // router.use('/races', require('./races'))
 // router.use('/candidates', require('./candidates'))
+router.use('/products', require('./products'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
