9e28624082cb2268dbbd4035fef1de1c16045900
Hystee <hysteebyhystee@gmail.com>
2018-10-31 21:14:14 -0400

    fix(store): Add basic order stor to get all orders
    Add Action types and creators, thunk creators, and reducer
    
    Closes #63

--- client/store/orders.js
+++ client/store/orders.js
@@ -1,6 +1,53 @@
-// import axios from 'axios'
+import axios from 'axios'
 
-// /**
-//  * ACTION TYPES
-//  */
-// const GET_ORDERS = 'GET_ORDERS'
+/**
+ * ACTION TYPES
+ */
+const GET_ORDERS = 'GET_ORDERS'
+
+/**
+ * INITIAL STATE
+ */
+
+const defaultOrderState = {
+  allOrders: []
+}
+
+/**
+ * ACTION CREATORS
+ */
+export const gotOrders = orders => ({
+  type: GET_ORDERS,
+  orders
+})
+
+/**
+ * THUNK CREATORS
+ */
+
+export const getOrders = () => {
+  return async dispatch => {
+    try {
+      const response = await axios.get('/api/orders')
+      const orders = response.data
+      const action = gotOrders(orders)
+      dispatch(action)
+    } catch (err) {
+      // to add toastr
+      console.error(err)
+    }
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = defaultOrderState, action) {
+  switch (action.type) {
+    case GET_ORDERS: {
+      return {...state, allOrders: action.orders}
+    }
+    default:
+      return state
+  }
+}
