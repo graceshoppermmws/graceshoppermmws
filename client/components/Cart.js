1a9feb7ff635a619c30630468e95495a78abf08c
hystee <29220493+hystee@users.noreply.github.com>
2018-11-05 17:39:40 -0500

    Subtotals #148 (#149)
    * feat(pastOrders): add past order route and state in redux store
    
    * feat(pastOrders): add past order route and state in redux store
    
    * (feat): deployment
    
    activate deployment
    
    closes #106
    
    * feat(add to cart) Add to cart and update quanitity route
    
    Add to cart and update quantity route
    
    Progress on #105
    
    * feat(checkout) Add checkout
    
    Route for checkout to set status as complete and create new cart
    
    Progress on #105
    
    * feat(checkout): Update to checkout
    
    Checkout button on cart page
    
    Progress on #105
    
    * feat(cart): Add subtotals to cart
    
    Closes #148
    
    * remove {}

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -90,9 +90,11 @@ class Cart extends Component {
                 handleDeleteProduct={this.handleDeleteProduct}
               />
             )}
-        <Elements>
-          <CheckoutForm handleCheckout={this.handleCheckout} />
-        </Elements>
+        
+          <Elements>
+            <CheckoutForm handleCheckout={this.handleCheckout} />
+          </Elements>
+        
       </div>
     )
   }
