68832ef3e150cf6dc5ecc2cae30e990f716616b2
Hystee <hysteebyhystee@gmail.com>
2018-11-05 17:31:01 -0500

    feat(cart): Add subtotals to cart
    Closes #148

--- client/components/Order.js
+++ client/components/Order.js
@@ -1,54 +1,69 @@
-import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react'
 
 const Order = props => {
   const {id, userId, isCart, isShipped, createdAt, products} = props.order
   const {isAdmin} = props.user || false
+  let subtotal = 0
+
+  products.forEach(product => {
+    const price = product.order_product
+      ? product.order_product.historicPrice
+      : product.price
+    const quantity = product.order_product
+      ? product.order_product.quantity
+      : product.quantity
+    subtotal += price * quantity
+  })
+
   return (
     <div>
       <h3>Order: {id}</h3>
-      <ul>
-        <li>User: {userId}</li>
-        <li>is Cart: {isCart ? 'true' : 'false'}</li>
-        <li>is Shipped: {isShipped ? 'true' : 'false'}</li>
-        <li>Created: {createdAt}</li>
-      </ul>
+      {isAdmin ? (
+        <ul>
+          <li>User: {userId}</li>
+          <li>is Cart: {isCart ? 'true' : 'false'}</li>
+          <li>is Shipped: {isShipped ? 'true' : 'false'}</li>
+          <li>Created: {createdAt}</li>{' '}
+        </ul>
+      ) : (
+        ' '
+      )}
+
       <ul>
         <li>
           <ul>
-            {products.map(product => (
-              <div key={product.id}>
-                <li>Name: {product.name}</li>
-                <li>
-                  Purchase Price:{' '}
-                  {product.order_product
-                    ? product.order_product.historicPrice
-                    : product.price}
-                </li>
-                <li>
-                  Quantity:{' '}
-                  {product.order_product
-                    ? product.order_product.quantity
-                    : product.quantity}
-                </li>
+            {products.map(product => {
+              const price = product.order_product
+                ? product.order_product.historicPrice
+                : product.price
+              const quantity = product.order_product
+                ? product.order_product.quantity
+                : product.quantity
+              return (
+                <div key={product.id}>
+                  <li>Name: {product.name}</li>
+                  <li>Purchase Price: {price}</li>
+                  <li>Quantity: {quantity}</li>
 
-                {isAdmin ? (
-                  ' '
-                ) : isCart ? (
-                  <button
-                    type="button"
-                    onClick={() =>
-                      props.handleDeleteProduct(userId, product.id)
-                    }
-                  >
-                    Remove Item From Cart
-                  </button>
-                ) : (
-                  ' '
-                )}
-              </div>
-            ))}
+                  {isAdmin ? (
+                    ' '
+                  ) : isCart ? (
+                    <button
+                      type="button"
+                      onClick={() =>
+                        props.handleDeleteProduct(userId, product.id)
+                      }
+                    >
+                      Remove Item From Cart
+                    </button>
+                  ) : (
+                    ' '
+                  )}
+                </div>
+              )
+            })}
           </ul>
+          Subtotal: ${subtotal}.00
         </li>
       </ul>
     </div>
