ec69b78aa0ada51797aceb1d4f6cc731875a187c
Kaitlin Maier <40339294+kaitlinmaier@users.noreply.github.com>
2018-11-05 13:00:55 -0500

    Filter category #134 (#136)
    * feat(filter): Add filter for multiple categories
    
    Progress on #134
    
    * feat(filter): Remove console log
    
    * feat(filter): Add documentation
    
    Add Documentation

---
+++ server/api/tags.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const {Product} = require('../db/models')
+module.exports = router
+
+// GET all tags of all products
+router.get('/', async (req, res, next) => {
+  try {
+    const allProductTags = await Product.allProductTags()
+    console.log('hello', allProductTags)
+    res.status(200).json(allProductTags)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// GET all tags of one product by productId
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const oneProduct = await Product.findById(+req.params.productId)
+    const productTags = await oneProduct.productTags()
+    res.status(200).json(productTags)
+  } catch (error) {
+    next(error)
+  }
+})
