841a6e9dc854b4998815c7f4f48dc521723a0c89
hystee <29220493+hystee@users.noreply.github.com>
2018-11-06 12:54:01 -0500

    formatting

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -104,10 +104,12 @@ class Cart extends Component {
       } else {
         cart = {isCart: true, products: []}
       }
-    } else if (this.state.cart.products) {
-      cart = this.state.cart
-    } else {
-      cart = {isCart: true, products: []}
+    } else { 
+      if (this.state.cart.products) {
+        cart = this.state.cart
+      } else {
+        cart = {isCart: true, products: []}
+      }
     }
     return (
       <div>
