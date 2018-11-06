af09b5df7923dcc28d4b9127faf9f091bdc1a93f
Hystee <hysteebyhystee@gmail.com>
2018-11-06 18:06:00 -0500

    feat(cart): Enable logged out user to checkout
    Closes #168

--- server/api/stripe.js
+++ server/api/stripe.js
@@ -8,28 +8,30 @@ router.use(require('body-parser').text())
 router.post('/charge', async (req, res) => {
   try {
     let calculateTotal = 2000
-    const userId = req.user.id || null
-    const cart = await Order.findOne({
-      where: {isCart: true, userId},
-      include: [{model: Product}]
-    })
-    // *** iterate through order products to find total
-    const {products} = cart
-    const productPromises = products.map(product =>
-      Product.findOne({where: {id: product.id}})
-    )
-    const dbProductsArray = await Promise.all(productPromises)
-    const updateJoinTablePromises = dbProductsArray.map(product =>
-      OrderProduct.findOne({
-        where: {orderId: cart.id, productId: product.id}
+    const userId = req.user ? req.user.id : null
+    if (userId) {
+      const cart = await Order.findOne({
+        where: {isCart: true, userId},
+        include: [{model: Product}]
       })
-    )
-    const joinTableArray = await Promise.all(updateJoinTablePromises)
-    calculateTotal = joinTableArray.reduce(
-      (subtotal, lineItem) =>
-        lineItem.historicPrice * lineItem.quantity * 100 + subtotal,
-      0
-    )
+      // *** iterate through order products to find total
+      const {products} = cart
+      const productPromises = products.map(product =>
+        Product.findOne({where: {id: product.id}})
+      )
+      const dbProductsArray = await Promise.all(productPromises)
+      const updateJoinTablePromises = dbProductsArray.map(product =>
+        OrderProduct.findOne({
+          where: {orderId: cart.id, productId: product.id}
+        })
+      )
+      const joinTableArray = await Promise.all(updateJoinTablePromises)
+      calculateTotal = joinTableArray.reduce(
+        (subtotal, lineItem) =>
+          lineItem.historicPrice * lineItem.quantity * 100 + subtotal,
+        0
+      )
+    }
     console.log(`$${calculateTotal / 100}.00`)
     //*****
     let {status} = await stripe.charges.create({
